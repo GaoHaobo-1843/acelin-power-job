c509ae93bc7a8bf807da4304c93613500abf99cf
gao.haobo <gao.haobo@iwhalecloud.com>
2022-12-05 22:45:25 +0800

    job-manage init

---
+++ job-manage/src/main/java/com/acelin/powerjob/JobManageApplication.java
@@ -0,0 +1,13 @@
+package com.acelin.powerjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobManageApplication.class, args);
+    }
+
+}
